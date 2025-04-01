3f390f750b9fde014f4f361cdeb47eac0797eefb
CreatorRamya <swarnareddy1983@gmail.com>
2025-04-01 17:37:38 +0100

    Add files via upload

---
+++ TupleOperations.py
@@ -0,0 +1,19 @@
+tuplex = ("tuple", False, 3.2, 2)
+print(tuplex)
+
+teplex = (4, 9, 6, 1, 4, 8, 2)
+print(tuplex)
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (20, 40, 70, 50, 80)
+print(tuple1.count(50))
+
+tuplex = (2, 5, 4, 7, 2, 8, 1 )
+
+_slice = tuplex[3:5]
+print(_slice)
+
+_slice = tuplex[:6]
+print(_slice)
