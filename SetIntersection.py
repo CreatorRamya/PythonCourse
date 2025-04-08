8c0c70f113e5982201be4aaf8f3cac87061f4c4b
CreatorRamya <swarnareddy1983@gmail.com>
2025-04-08 17:31:23 +0100

    Add files via upload

---
+++ SetIntersection.py
@@ -0,0 +1,13 @@
+setx = {"pink", "purple"}
+sety = {"purple", "orange"}
+print("Orginal set elements: ")
+
+print(setx)
+print(sety)
+print("\nIntersection of two said sets: ")
+setz = setx.intersection(sety)
+print(setz)
+
+print("\nUnion of two said sets: ")
+setz = setx.union(sety)
+print(setz)
