0194737e4de9d63e8eeeab2b5623002ecf161792
CreatorRamya <swarnareddy1983@gmail.com>
2025-05-06 17:42:01 +0100

    Add files via upload

---
+++ FruitQuiz.py
@@ -0,0 +1,28 @@
+import random
+
+class FruitQuiz:
+
+    def __init__(self):
+
+        self.fruits={'strawberry' : 'red', 'avocado' : 'green', 'watermelon': 'dark green', 'kiwi': 'brown'}
+
+    def quiz(self):
+        while (True):
+            fruit, color = random.choice(list(self.fruits.items()))
+
+            print("What is the color of {}".format(fruit))
+            user_answer = input()
+
+            if(user_answer.lower() == color):
+                print("Correct answer")
+
+            else:
+                print("Wrong answer")
+
+            option = int(input("enter 0, if you would like to play again or else enter 1: "))
+            if (option):
+                break
+
+print("Welcome to the Fruit Quiz!")
+fq = FruitQuiz()
+fq.quiz()
